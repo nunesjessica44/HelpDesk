220d6b76cacf6b07a0e0ae311b5cda8ffe1a9eb7
unknown <vitoriasankhyaagir@gmail.com>
2022-06-09 21:43:50 -0300

    Comandos dos formulario

--- resources/views/formularioChamado.blade.php
+++ resources/views/formularioChamado.blade.php
@@ -6,7 +6,35 @@
 
         <title>Formulario Chamado</title>
     </head>
-    <body >
-        <h1>Formulario Chamado</h1>
-    </body>
+    <body>
+    <h1>Formulario Chamado</h1>
+
+    section>
+    <form action="{{url('/contato/inserir')}}" method="post">
+        {{csrf_field()}}
+        <div>
+            <input type="text" name="txNome" placeholder="Nome" value="nome teste" />
+        </div>
+
+        <div>
+            <input type="text" name="txEmail" placeholder="E-mail" value="teste email" />
+        </div>
+
+        <div>
+            <input type="text" name="txFone" placeholder="Telefone" value="fone teste" />
+        </div>
+
+        <div>
+            <input type="text" name="txAssunto" placeholder="Assunto" value="assunto teste" />
+        </div>
+
+        <div>
+            <input type="text" name="txMensagem" placeholder="Mensagem" value="msg teste" />
+        </div>
+
+        <div>
+            <input type="submit" value="Enviar" />
+        </div>
+    </form>
+
 </html>
